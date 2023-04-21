6b370bc845fcd64a4701329040bc21b0ad51c336
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-04-22 00:29:55 +0530

    Displaying Dataframe 5

--- .history/Main_20230422002936.py
+++ .history/Main_20230422002936.py
@@ -7,7 +7,7 @@ def app(show):
     SheetLink = st.text_input('Enter the Link of Google Sheets :')
     st.write(SheetLink)
     sheetlink = 'https://docs.google.com/spreadsheets/d/1SS5CyRoFk9t7lm7ZYg0vup1Nkp1NJqUV4j3Y1r8J5dM/export?foramt=csv'
-    df = pd.read_csv(sheetlink)
+    df = pd.read_csv(SheetLink)
     print(df)
 
 if __name__ == '__main__' :
